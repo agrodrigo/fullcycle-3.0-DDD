68cd0bca957ec51d8fb78dd2546090e55d036b3b
Rodrigo Pinto Pereira <rodrigo@geogis.com.br>
2022-03-26 14:45:30 -0400

    feat: princípio de autovalidação -> customer

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -1,14 +1,23 @@
 class Customer {
   _id: string;
   _name: string;
-  _address: string;
+  _address?: string;
   _active: boolean;
 
-  constructor(id: string, name: string, address: string){
+  constructor(id: string, name: string){
     this._id = id;
     this._name = name;
-    this._address = address;
     this._active = false;
+    this.validate();
+  }
+
+  validate() {
+    if (this._name.length === 0) {
+      throw new Error('Name is required');
+    }
+    if (this._id.length === 0) {
+      throw new Error('Id is required');
+    }
   }
 
   changeName(name: string) {
@@ -16,6 +25,9 @@ class Customer {
   }
 
   activate() {
+    if (this._address?.length === 0) {
+      throw new Error('Address is mandatory to activate a customer');
+    }
     this._active = true;
   }
 
