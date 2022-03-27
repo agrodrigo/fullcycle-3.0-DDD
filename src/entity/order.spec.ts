ccf3de4087e6457c45dbe307abb2637784438386
Rodrigo Pinto Pereira <rodrigo@geogis.com.br>
2022-03-26 22:24:11 -0400

    feat: relacionando item com Product

--- src/entity/order.spec.ts
+++ src/entity/order.spec.ts
@@ -22,19 +22,30 @@ describe("Order unit tests", () => {
   })
 
   it('shoud calculate total', () => {
-    const item1 = new OrderItem('i1', 'Item 1', 100);
-    const item2 = new OrderItem('i2', 'Item 2', 100);
+    const item1 = new OrderItem('i1', 'Item 1', 100, 'p1', 2);
+    const item2 = new OrderItem('i2', 'Item 2', 100, 'p1', 2);
     const order = new Order('o1', 'c1', [item1])
     
     let total = order.total();
     
-    expect(total).toBe(100)
+    expect(total).toBe(200)
     
     const order2 = new Order('o1', 'c1', [item1, item2])
 
     total = order2.total();
 
-    expect(total).toBe(200)
+    expect(total).toBe(400)
 
   })
+
+  it('shoud throw error if the item qtd is less or equal zero', () => {
+
+    expect(() => {
+      const item1 = new OrderItem('i1', 'Item 1', 100, 'p1', 0);
+      const order = new Order('o1', 'c1', [item1]);
+
+    }).toThrowError('Quantity must be greater than 0');
+    
+  })
+
 })
