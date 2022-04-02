99bd91d7bcdd9b1fae9d50e603ed2f73f980803c
Rodrigo Pinto Pereira <rodrigo@geogis.com.br>
2022-04-02 14:42:08 -0400

    feat: excluir registro de um evento

--- src/domain/event/@shared/event-dispatcher.spec.ts
+++ src/domain/event/@shared/event-dispatcher.spec.ts
@@ -11,5 +11,23 @@ describe('Domain events tests', () => {
 
     expect(eventDispather.getEventHandlers['ProductCreatedEvent']).toBeDefined();
     expect(eventDispather.getEventHandlers['ProductCreatedEvent'].length).toBe(1);
+    expect(eventDispather.getEventHandlers['ProductCreatedEvent'][0]).toMatchObject(eventHandler);
+  })
+
+  it('should unregister an aevent handler', () => {
+
+    const eventDispather = new EventDispatcher();
+    const eventHandler = new SendEmailWhenProductIsCreatedHandler();
+
+    eventDispather.register('ProductCreatedEvent', eventHandler)
+
+    expect(eventDispather.getEventHandlers['ProductCreatedEvent'][0]).toMatchObject(eventHandler);
+
+    eventDispather.unregister('ProductCreatedEvent', eventHandler);
+
+    expect(
+      eventDispather.getEventHandlers['ProductCreatedEvent']
+    ).toBeDefined();
+    expect(eventDispather.getEventHandlers['ProductCreatedEvent'].length).toBe(0);
   })
 })
--- src/domain/event/@shared/event-dispatcher.ts
+++ src/domain/event/@shared/event-dispatcher.ts
@@ -23,7 +23,12 @@ export default class EventDispatcher implements EventDispatcherInterface {
   }
 
   unregister(eventName: string, eventHandler: eventHandlerInterface<eventInterface>): void {
-    
+    if(this.eventHandlers[eventName]) {
+      const index = this.eventHandlers[eventName].indexOf(eventHandler);
+      if (index !== -1) {
+        this.eventHandlers[eventName].splice(index, 1);
+      }
+    }
   }
 
   unregisterAll(): void {
